1bb6ba0e44dc2bc33048750cc32dac440933b1ab
Heloísa Saldanha <65875963+heloisaldanha@users.noreply.github.com>
2021-03-04 16:36:28 -0300

    Delete blog directory

--- blog/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# HttpResponse é só pra dar uma resposta na tela
-# views é onde cria o método que acontece na página
-
-
-# sempre vem essas requests por convenção
-def index(request):
-    return HttpResponse('Olá mundo!')
